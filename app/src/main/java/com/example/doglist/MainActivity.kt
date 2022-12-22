bfba27053b231fb94c50f5cba2b443cfbc4ab097
santiago ramirez cano <santiago.ramirezc26@gmail.com>
2022-12-22 18:54:20 -0500

    Initialized Retrofit instance and created the function searchByName which will performed the retrofit's call and then an if else statement.

--- app/src/main/java/com/example/doglist/MainActivity.kt
+++ app/src/main/java/com/example/doglist/MainActivity.kt
@@ -3,6 +3,12 @@ package com.example.doglist
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.doglist.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,4 +19,24 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
+
+    private fun getRetrofit() : Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://dog.ceo/api/breed/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    private fun searchByName(query:String){
+        CoroutineScope(Dispatchers.IO).launch {
+            val call = getRetrofit().create(APIService::class.java).getDogsByBreeds("$query/images")
+            val puppies = call.body()
+                if(call.isSuccessful) {
+                    //show Recyclerview
+                }else{
+                    //show error
+                }
+        }
+    }
+
 }
